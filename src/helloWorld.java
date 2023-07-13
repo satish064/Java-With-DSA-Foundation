d695bc1b32fef26169327a5f99a1c0058e62dbaf
satish064 <satishpaul064@gmail.com>
2023-07-13 21:06:46 +0530

    Loop problem(Fibonacci Series)

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,13 +1,20 @@
+
+import java.util.Scanner;
 public class helloWorld
+
 {
     public static void main(String[] args)
     {
-        int i = 1,sum = 0;
-        do
+        // 123 321
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+        System.out.println("Enter b number :  ");
+        int sum = sc.nextInt(),ans = 1;
+        for(int i = 1;i<=sum;i++)
         {
-            sum += i;
-            i++;
-        }while(i<=10);
-        System.out.println(sum);
+           ans *= num;
+        }
+        System.out.println(ans);
     }
 }
