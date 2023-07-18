0091eeda497fc5033b462605c5f3faae93d445af
satish064 <satishpaul064@gmail.com>
2023-07-18 22:23:49 +0530

    OOPs Introduction

---
+++ src/Loops/Student.java
@@ -0,0 +1,12 @@
+package Loops;
+public class Student{
+    String name = "John";
+    int Roll_no = 2;
+}
+class main {
+    public static void main(String[] args) {
+        Student stu = new Student();
+        System.out.println(stu.name);
+        System.out.println(stu.Roll_no);
+    }
+}
---
+++ src/oops/main1.java
@@ -0,0 +1,19 @@
+package oops;
+
+//public class Person {
+//
+//}
+
+
+class Person
+{
+    int age;
+}
+public class main1
+{
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.age = 20;
+        System.out.println(p1.age);
+    }
+}
