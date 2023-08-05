d3c79496d284e3e73bf58d0fa806314f274813dc
satish064 <satishpaul064@gmail.com>
2023-08-05 18:05:37 +0530

    Methods

---
+++ src/Methods/add.java
@@ -0,0 +1,23 @@
+package Methods;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+import java.util.Scanner;
+
+class Algebra{
+    int add(int a,int b)
+    {
+        int ans = a + b;
+        return ans;
+    }
+}
+public class add {
+    public static void main(String[] args) {
+        Algebra obj = new Algebra();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum of input numbers is: ");
+       int ans =  obj.add(a,b);
+        System.out.println("Sum is :"+ans);
+
+    }
+}
